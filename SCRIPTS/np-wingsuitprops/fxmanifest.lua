a247f113663ca53d919dc8f2213b9512f0ba7790
unknown3211 <100625853+unknown3211@users.noreply.github.com>
2023-01-23 04:08:08 -0600

    Added np-wingsuit Props

---
+++ SCRIPTS/np-wingsuitprops/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "adamant"
+
+game "gta5"
+
+files {
+    "stream/np_wingsuit_b_closed.ydr",
+    "stream/np_wingsuit_b_open.ydr",
+    "stream/np_wingsuit_closed.ydr",
+    "stream/np_wingsuit_open.ydr",
+    "stream/np_wingsuit+hidr.ytd"
+}
+
+data_file "DLC_ITYP_REQUEST" "stream/np_wingsuit.ytyp"
